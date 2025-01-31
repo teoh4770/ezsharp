bb23d7820a57459b4034ce18b7e07a11874c2aef
Chee Kian Teoh <teoh4770@mylaurier.ca>
2025-01-31 00:45:09 -0500

    fix: token now receive start pointer and lenght instead of value

--- scanner/token.c
+++ scanner/token.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include "token.h"
 
-Token makeToken(TokenType type, char *value, int line)
+//> make-token
+Token makeToken(TokenType type, char *start, int length, int line)
 {
   Token token;
   token.type = type;
-  token.value = value;
+  token.start = start;
+  token.length = length;
   token.line = line;
 
   return token;
 }
+//< make-token
 
+//> print-token
 void printToken(Token *token)
 {
-  printf("The token is at line %d\n", token->line);
+  printf("The token starts at line %d\n", token->line);
   printf("The token type is %d\n", token->type);
-  printf("The token value is %s\n", token->value);
+  printf("length: %d\n", token->length);
+  char *startCharacter = token->start;
+  for (int i = 0; i < token->length; i++)
+  {
+    if (i == token->length - 1)
+    {
+      printf("%c\n", *startCharacter);
+    }
+    else
+    {
+      printf("%c", *startCharacter);
+    }
+
+    startCharacter++;
+  }
 }
+//< print-token
--- scanner/token.h
+++ scanner/token.h
@@ -1,41 +1,44 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+// Note: number is for debugging purpose, remove later on
 typedef enum
 {
-  TOKEN_ADD,
-  TOKEN_SUB,
-  TOKEN_MUL,
-  TOKEN_DIV,
-  TOKEN_MOD,
-  TOKEN_COMMA,
-  TOKEN_LEFT_PAREN,
-  TOKEN_RIGHT_PAREN,
-  TOKEN_LEFT_SQUARE_PAREN,
-  TOKEN_RIGHT_SQUARE_PAREN,
-  TOKEN_SEMICOLON,
-  TOKEN_DOT,
-  TOKEN_ASSIGN_OP,
-  TOKEN_LT,
-  TOKEN_LE,
-  TOKEN_GT,
-  TOKEN_GE,
-  TOKEN_EQ,
-  TOKEN_NE,
-  TOKEN_INT,
-  TOKEN_DOUBLE,
-  TOKEN_ID,
-  TOKEN_KEYWORD
+  TOKEN_ADD = 0,
+  TOKEN_SUB = 1,
+  TOKEN_MUL = 2,
+  TOKEN_DIV = 3,
+  TOKEN_MOD = 4,
+  TOKEN_COMMA = 5,
+  TOKEN_LEFT_PAREN = 6,
+  TOKEN_RIGHT_PAREN = 7,
+  TOKEN_LEFT_SQUARE_PAREN = 8,
+  TOKEN_RIGHT_SQUARE_PAREN = 9,
+  TOKEN_SEMICOLON = 10,
+  TOKEN_DOT = 11,
+  TOKEN_ASSIGN_OP = 12,
+  TOKEN_LT = 13,
+  TOKEN_LE = 14,
+  TOKEN_GT = 15,
+  TOKEN_GE = 16,
+  TOKEN_EQ = 17,
+  TOKEN_NE = 18,
+  TOKEN_INT = 19,
+  TOKEN_DOUBLE = 20,
+  TOKEN_ID = 21,
+  TOKEN_KEYWORD = 22,
+  TOKEN_WHITESPACE = 23,
 } TokenType;
 
 typedef struct
 {
   TokenType type;
-  char *value;
-  int line;
+  char *start;   // Start of lexeme
+  int length;    // Size of lexeme
+  int line;      
 } Token;
 
-Token makeToken(TokenType type, char *value, int line);
+Token makeToken(TokenType type, char *start, int length, int line);
 void printToken(Token *token);
 
 #endif
